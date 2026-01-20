9af8137a6e41d53031c4f0e276b148791acd6890
Ojude2025 <jude.okozi@gmail.com>
2026-01-20 14:24:56 +0000

    main

--- cypress/e2e/PageObjects/Blog/DeleteBlog_StepDef.js
+++ cypress/e2e/PageObjects/Blog/DeleteBlog_StepDef.js
@@ -8,13 +8,11 @@ const blogAction = (id) => `[data-testid="blog-actions-${id}"]`;
 const deleteIcon = (id) => `[data-testid="delete-blog-${id}"]`;
 
 Given("that I am on the Blog post", () => {
-  // cy.get('[data-testid="menu-blog"]');
   cy.url().should("include", "/admin-dashboard");
-   cy.get('[data-testid="option-icon-blog"]').click()
+  cy.get('[data-testid="option-icon-blog"]').click()
 });
 
 And("have selected a blog content", () => {
-  // cy.get('[data-testid="blog-submenu"]').click();
   cy.get('[data-testid="submenu-all-blogs"]').click();
   cy.get(blogTable).should("exist").and("be.visible");
   cy.get(blogRow(id)).should("exist").and("be.visible");
@@ -27,6 +25,5 @@ When("I click on delete", () => {
 });
 
 Then("I can delete a blog content that is unwanted on the website", () => {
-  // cy.get(blogRow(id)).should("not.exist");
   cy.url().should("include", "/blog/all");
 });
